f54aa7b27216eedf2444deba874e8564702968fe
GrettoRosella <rezarifaldy12@gmail.com>
2021-06-13 09:41:31 +0000

    surya: tmp kernel stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 TARGET_KERNEL_CONFIG := surya-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 TARGET_KERNEL_USE_LATEST_CLANG := true
-TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_ADDITIONAL_FLAGS += AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
